057226adb2d4c3f2c972f08d91265f34b30d442e
CHEN Junyi <junyichen8@ad.cityu.edu.hk>
2017-10-18 11:44:36 +0800

    EDIT CNN.PY

--- CNN.py
+++ CNN.py
@@ -1,17 +1,13 @@
-
 # coding: utf-8
 
 # In[24]:
-
 
 #import pip
 
 #package_name='shutil'
 #pip.main(['install', package_name])
 
-
 # In[1]:
-
 
 import os
 import re
@@ -36,78 +32,12 @@ from keras.preprocessing.text import Tokenizer
 from keras.utils.np_utils import to_categorical
 from sklearn.metrics import recall_score
 from sklearn.metrics import precision_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
 
-
-argvs= sys.argv
+argvs = sys.argv
 
 CID = sys.argv[1]
-
-
-# In[2]:
-
-
-data = pd.read_csv(os.path.join('data','01.csv'),encoding = "ISO-8859-1")
-text = data['text']
-sentiment = data['target']
-
-
-# In[3]:
-
-
-print(text.size)
-
-
-# In[4]:
-
-
-data['text'] = data['text'].apply(lambda x: x.lower())
-data['text'] = data['text'].apply((lambda x: re.sub('[^a-zA-z0-9\s]','',x)))
-
-max_fatures = 2000
-tokenizer = Tokenizer(nb_words=max_fatures, split=' ')
-
-text_list = [s.encode('ascii') for s in text.values]
-
-tokenizer.fit_on_texts([s.encode('ascii') for s in text_list])
-X = tokenizer.texts_to_sequences(text_list)
-X = pad_sequences(X)
-
-
-# In[5]:
-
-
-X2 = tokenizer.texts_to_matrix(text_list,mode="tfidf")
-
-
-# In[6]:
-
-
-
-
-# In[7]:
-
-
-X3 = [np.reshape(X2[i], (-1, 20))for i in range(0,len(X2))]
-X3 = np.asarray(X3)
-#print(X3.shape)
-
-
-# In[8]:
-
-
-#X2.shape
-
-
-# In[9]:
-
-
-#X.shape
-
-
-# There are 220 postitive text and 563 negative text
-
-# In[10]:
-
 
 def precision(y_true, y_pred):
     #Calculates the precision
@@ -124,162 +54,145 @@ def recall(y_true, y_pred):
     recall = true_positives / (possible_positives + K.epsilon())
     return recall
 
-def precision1(y_true,y_pred):
-    return precision_score(y_true,y_pred)
-def recall1(y_true,y_pred):
-    return recall_score(y_true,y_pred)
+
+def precision1(y_true, y_pred):
+    return precision_score(y_true, y_pred)
 
 
+def recall1(y_true, y_pred):
+    return recall_score(y_true, y_pred)
 
-model = Sequential()
-#model.add(LSTM(512, return_sequences=True, input_shape=X3[0].shape, dropout=0.2, recurrent_dropout=0.2))
-model.add(GRU(512, return_sequences=True, input_shape=X3[0].shape, dropout=0.2, recurrent_dropout=0.2))
-model.add(GRU(512, return_sequences=False, dropout=0.2))
-#model.add(LSTM(512, return_sequences=False, dropout=0.2))
+def get_data():
+    data = pd.read_csv(os.path.join('data', '01.csv'), encoding="ISO-8859-1")
+    text = data['text']
+    sentiment = data['target']
+    data['text'] = data['text'].apply(lambda x: x.lower())
+    data['text'] = data['text'].apply(
+        (lambda x: re.sub('[^a-zA-z0-9\s]', '', x)))
+    max_fatures = 2000
+    tokenizer = Tokenizer(nb_words=max_fatures, split=' ')
+    text_list = [s.encode('ascii') for s in text.values]
+    tokenizer.fit_on_texts([s.encode('ascii') for s in text_list])
+    X = tokenizer.texts_to_sequences(text_list)
+    X = pad_sequences(X)
+    X2 = tokenizer.texts_to_matrix(text_list, mode="tfidf")
+    X3 = [np.reshape(X2[i], (-1, 20)) for i in range(0, len(X2))]
+    X3 = np.asarray(X3)
 
-#model.add(Embedding(max_fatures, 512,input_length = X.shape[1], dropout=0.2))
-#model.add(LSTM(512, dropout_U=0.2, dropout_W=0.2))
-model.add(Dense(2,activation='softmax'))
-model.compile(loss = 'categorical_crossentropy', optimizer='adam',metrics=['accuracy',recall,precision])
-print(model.summary())
+    Y = [sentiment[i] for i in range(0, len(sentiment))]
+    Y = np.asarray(Y)
 
+    ohenc= OneHotEncoder()
+    Y2 = ohenc.fit_transform(Y).toarray()
 
-# In[11]:
-
-
-type(X)
-
-
-# In[12]:
-
-
-X = X3
-
-
-# In[13]:
-
-
-Y = [sentiment[i]for i in range(0,len(sentiment))]
-Y = np.asarray(Y)
-
-#
-X, Y = shuffle(X, Y)
-X_train = X[:600]
-X_test = X[600:]
-Y2 = []
-for i in range(0,len(Y)):
-    if Y[i]==1:
-        Y2.append([Y[i],0])
-    else:
-        Y2.append([Y[i],1])
-Y2 = np.asarray(Y2)
-Y_train= Y2[:600]
-Y_test = Y2[600:]
-#print(Y_test.shape)
-
-
-# In[14]:
-
-
-X.shape
-
-
-# In[15]:
-
+    X_train,X_test,Y_train,Y_test = train_test_split(X3,Y2,test_size=0.3)
+    return X_train,X_test,Y_train,Y_test,X,X2,X3
 
 class TestCallback(Callback):
-    def __init__(self, test_data1,test_data2):
+    def __init__(self, test_data1, test_data2):
         self.Xdata = test_data1
         self.Ydata = test_data2
 
     def on_epoch_end(self, epoch, logs={}):
-        x = self.Xdata 
+        x = self.Xdata
         y = self.Ydata
-        loss, acc, recall,pre = self.model.evaluate(x, y, verbose=0)
+        loss, acc, recall, pre = self.model.evaluate(x, y, verbose=0)
         print('\nTesting loss: {}, acc: {}\n'.format(loss, acc))
-batch_size = 32
-checkpointer = ModelCheckpoint(filepath=os.path.join('tmp','weights_'+CID+'.hdf5'), verbose=1, save_best_only=True)
-model.fit(X_train, Y_train, nb_epoch = 10, batch_size=batch_size, verbose = 1,validation_data=(X_test, Y_test), callbacks=[checkpointer])
-#callbacks=[TestCallback(X_test, Y_test),checkpointer]
+
+def main():
+
+    X_train,X_test,Y_train,Y_test,X,X2,X3 = get_data()
+    model = Sequential()
+    model.add(
+        GRU(512,
+        return_sequences=True,
+        input_shape=X3[0].shape,
+        dropout=0.2,
+        recurrent_dropout=0.2))   
+    model.add(GRU(512, return_sequences=False, dropout=0.2))
+    model.add(Dense(2, activation='softmax'))
+    model.compile(
+        loss='categorical_crossentropy',
+        optimizer='adam',
+        metrics=['accuracy', recall, precision])
+    print(model.summary())
+
+    batch_size = 32
+
+    checkpointer = ModelCheckpoint(
+        filepath=os.path.join('tmp', 'weights_' + CID + '.hdf5'),
+        verbose=1,
+        save_best_only=True)
+    
+    model.fit(
+        X_train,
+        Y_train,
+        nb_epoch=10,
+        batch_size=batch_size,
+        verbose=1,
+        validation_data=(X_test, Y_test),
+        callbacks=[checkpointer])
+    
+    newData = X_test.reshape(183, 1, 100, 20)
+
+    model = Sequential()
+    model.add(
+        GRU(512,
+        return_sequences=True,
+        input_shape=X3[0].shape,
+        dropout=0.2,
+        recurrent_dropout=0.2))
+    
+    model.add(GRU(512, return_sequences=False, dropout=0.2))
+    model.add(Dense(2, activation='softmax'))
+    model.load_weights(filepath=os.path.join('tmp', 'weights_' + CID + '.hdf5'))
+    model.compile(
+        loss='categorical_crossentropy',
+        optimizer='adam',
+        metrics=['accuracy', recall, precision])
+    print(model.summary())
+    model.save_weights(filepath=os.path.join('tmp', 'weights_' + CID + '.hdf5'))
 
 
-# In[16]:
+    pcount, ncount, ppredict, npredict = 0, 0, 0, 0
+    for x in range(len(newData)):
+        result = model.predict(newData[x])[0]
+        if np.argmax(result) == np.argmax(Y_test[x]):
+            if np.argmax(Y_test[x]) == 0:
+                ppredict += 1
+            else:
+                npredict += 1
 
-
-X_test.shape
-
-
-# In[17]:
-
-
-type(X_test[0])
-newData = X_test.reshape(183,1,100,20)
-print(newData.shape)
-
-
-# In[18]:
-
-
-model = Sequential()
-#model.add(LSTM(512, return_sequences=True, input_shape=X3[0].shape, dropout=0.2, recurrent_dropout=0.2))
-#model.add(LSTM(512, return_sequences=False, dropout=0.2))
-model.add(GRU(512, return_sequences=True, input_shape=X3[0].shape, dropout=0.2, recurrent_dropout=0.2))
-model.add(GRU(512, return_sequences=False, dropout=0.2))
-#model.add(Embedding(max_fatures, 512,input_length = X.shape[1], dropout=0.2))
-#model.add(LSTM(512, dropout_U=0.2, dropout_W=0.2))
-model.add(Dense(2,activation='softmax'))
-model.load_weights(filepath=os.path.join('tmp','weights_'+CID+'.hdf5'))
-model.compile(loss = 'categorical_crossentropy', optimizer='adam',metrics=['accuracy',recall,precision])
-print(model.summary())
-model.save_weights(filepath=os.path.join('tmp','weights_'+CID+'.hdf5'))
-
-
-
-# In[19]:
-
-
-pcount, ncount, ppredict, npredict = 0, 0, 0, 0
-for x in range(len(newData)):  
-    result = model.predict(newData[x])[0]   
-    if np.argmax(result) == np.argmax(Y_test[x]):
         if np.argmax(Y_test[x]) == 0:
-            ppredict += 1
+            pcount += 1
         else:
-            npredict += 1
-       
-    if np.argmax(Y_test[x]) == 0:
-        pcount += 1
-    else:
-        ncount += 1
+            ncount += 1
 
-print("True 1:\t", ppredict/pcount)
-print("False 1:\t", 1-ppredict/pcount)
-print("True 0:\t", npredict/ncount)
-print("False 0:\t", 1-npredict/ncount)
+    print("True 1:\t", ppredict / pcount)
+    print("False 1:\t", 1 - ppredict / pcount)
+    print("True 0:\t", npredict / ncount)
+    print("False 0:\t", 1 - npredict / ncount)
 
-plt.switch_backend('agg')
-mp.use('Agg')
+    plt.switch_backend('agg')
+    mp.use('Agg')
 
+    cm = [[npredict / ncount, 1 - npredict / ncount],
+    [ppredict / pcount, 1 - ppredict / pcount]]
+    labels = ['0', '1']
 
-cm =  [[npredict/ncount, 1-npredict/ncount],
-       [ppredict/pcount, 1-ppredict/pcount]]
-labels = ['0', '1']
-
-
-fig, ax = plt.subplots()
-h = ax.matshow(cm)
-fig.colorbar(h)
-ax.set_xticklabels([''] + labels)
-ax.set_yticklabels([''] + labels)
-ax.set_xlabel('Ground truth')
-ax.set_ylabel('Predicted')
-plt.savefig(os.path.join('figures',str(argvs[1])+'.jpg'))
-plt.show()
-plt.close()
+    fig, ax = plt.subplots()
+    h = ax.matshow(cm)
+    fig.colorbar(h)
+    ax.set_xticklabels([''] + labels)
+    ax.set_yticklabels([''] + labels)
+    ax.set_xlabel('Ground truth')
+    ax.set_ylabel('Predicted')
+    plt.savefig(os.path.join('figures', str(argvs[1]) + '.jpg'))
+    plt.show()
+    plt.close()
+    return
 
 
-# In[28]:
+if __name__ == "__main__":
+    main()
 
-
-#import shutil
-#shutil.copy('/tmp/weights.hdf5', 'C:\\Users\\wongw\\Desktop')
