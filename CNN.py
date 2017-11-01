0ee161b06efd295f29bfa64c5d8e46904a6cb8a2
CHEN Junyi <junyichen8@ad.cityu.edu.hk>
2017-11-01 14:25:23 +0800

    get data 2

--- CNN.py
+++ CNN.py
@@ -38,6 +38,14 @@ from sklearn.preprocessing import OneHotEncoder
 from optparse import OptionParser
 from sklearn.metrics import classification_report
 
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import nltk
+
+
+words = set(nltk.corpus.words.words())
+
 argvs = sys.argv
 
 opts, args = {}, []
@@ -45,7 +53,28 @@ opts, args = {}, []
 print(argvs)
 print("##########")
 
+def removeNonEnglish(s):
+    sen = s
+    print(" ".join(w for w in nltk.wordpunct_tokenize(sen) if w.lower() in words or not w.isalpha()))
+    return " ".join(w for w in nltk.wordpunct_tokenize(sen) if w.lower() in words or not w.isalpha())
 
+def removeStopWords(ss):
+    stop_words = set(stopwords.words('english'))
+    tokens = word_tokenize(ss)
+    sent = [w for w in tokens if not w in stop_words]
+    sent2 = []
+    for w in tokens:
+        if w not in stop_words:
+            sent2.append(w)
+    return " ".join(sent2)
+
+def lemmat(ss):
+    lem = WordNetLemmatizer()
+    stop_words = set(stopwords.words('english'))
+    tokens = word_tokenize(ss)
+    sent = [lem.lemmatize(tokens[i],'n')for i in range(len(tokens))]
+    sent2 = [lem.lemmatize(sent[i],'v')for i in range(len(sent))]
+    return " ".join(sent2)
 def precision(y_true, y_pred):
     #Calculates the precision
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
@@ -79,6 +108,40 @@ def get_data():
     data['text'] = data['text'].apply(lambda x: x.lower())
     data['text'] = data['text'].apply(
         (lambda x: re.sub('[^a-zA-z0-9\s]', '', x)))
+    max_fatures = 2000
+    tokenizer = Tokenizer(num_words=max_fatures, split=' ')
+    text_list = [str(s.encode('ascii')) for s in text.values]
+    tokenizer.fit_on_texts(text_list)
+    X = tokenizer.texts_to_sequences(text_list)
+    X = pad_sequences(X)
+    X2 = tokenizer.texts_to_matrix(text_list, mode="tfidf")
+    X3 = [np.reshape(X2[i], (-1, 20)) for i in range(0, len(X2))]
+    X3 = np.asarray(X3)
+    X3 = X3.reshape(X3.shape[0],X3.shape[1],X3.shape[2],1)
+
+    Y = [sentiment[i] for i in range(0, len(sentiment))]
+    Y = np.asarray(Y)
+
+    ohenc = OneHotEncoder()
+    Y2 = ohenc.fit_transform(Y.reshape(-1, 1)).toarray()
+
+    X_train, X_test, Y_train, Y_test = train_test_split(X3, Y2, test_size=0.4)
+    return X_train, X_test, Y_train, Y_test, X, X2, X3, ohenc
+
+
+def get_data2():
+    data = pd.read_csv(os.path.join('data', '01.csv'), encoding="ISO-8859-1")
+    text = data['text']
+    sentiment = data['target']
+    data.head()
+    text = text.apply(lambda x: x.lower()) #lowercase
+    text = text.apply((lambda x: re.sub(r'[?|$|&|*|%|@|(|)|~]','',x))) #remove punctuations
+    text = text.apply((lambda x: re.sub(r'[^a-zA-Z0-9 ]','',x))) #only numbers and alphabet
+    text = text.apply(lambda x:removeStopWords(x)) #remove stopwords
+    text = text.apply(lambda x:lemmat(x)) #lemmatize the sentence
+
+    text = text.apply(lambda x: removeNonEnglish(x)) #remove
+
     max_fatures = 2000
     tokenizer = Tokenizer(num_words=max_fatures, split=' ')
     text_list = [str(s.encode('ascii')) for s in text.values]
@@ -181,7 +244,7 @@ def main():
 
     if (opts.load != 'none'): CID = opts.load
 
-    X_train, X_test, Y_train, Y_test, X, X2, X3, enc = get_data()
+    X_train, X_test, Y_train, Y_test, X, X2, X3, enc = get_data2()
 
     model = bulid_model(
         X_train, X_test, Y_train, Y_test, X, X2, X3, CID, fromfile=opts.load)
