8c79056f755127bb172c94b86b2fd8653be2bc9c
CHEN Junyi <junyichen8@ad.cityu.edu.hk>
2018-02-06 16:56:42 +0800

    test callback

--- CNN_MAX_TEST_LC.py
+++ CNN_MAX_TEST_LC.py
@@ -119,6 +119,8 @@ def bulid_model(X_train,
             verbose=1,
             save_best_only=True)
 
+        history = TestCallback()
+
         model.fit(
             X_train,
             Y_train,
@@ -126,7 +128,7 @@ def bulid_model(X_train,
             batch_size=batch_size,
             verbose=1,
             validation_data=(X_test, Y_test),
-            callbacks=[checkpointer])
+            callbacks=[checkpointer，history])
 
         model.save_weights(
             filepath=os.path.join('tmp', 'weights_' + CID + '.hdf5'))
