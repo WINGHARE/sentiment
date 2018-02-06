566b4b63e5b912482a277259500ed06056c81375
CHEN Junyi <junyichen8@ad.cityu.edu.hk>
2018-02-06 16:08:03 +0800

    self.modelname

--- CNN_MAX_TEST_LC.py
+++ CNN_MAX_TEST_LC.py
@@ -77,7 +77,7 @@ class TestCallback(Callback):
         x = self.Xdata
         y = self.Ydata
         loss, acc, recall, pre, f1 = self.model.evaluate(x, y, verbose=0)
-        print(model.metrics_names)
+        print(self.model.metrics_names)
         print('\nTesting loss: {}, acc: {}, recall: {}, preci: {}, f1: {}\n'.format(loss, acc, recall, pre, f1))
 
 
