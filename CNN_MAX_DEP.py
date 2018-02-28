6623481cc5e03fa32a473d468b17828a97011e5b
CHEN Junyi <junyichen8@ad.cityu.edu.hk>
2018-02-28 11:35:01 +0800

    edit 3 layers

--- CNN_MAX_DEP.py
+++ CNN_MAX_DEP.py
@@ -107,8 +107,17 @@ def bulid_model(X_train,
             padding='same',
             activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(
+        Conv2D(
+            32,
+            kernel_size=(10, 2),
+            strides=(1, 1),
+            padding='same',
+            activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
     model.add(Flatten())
     model.add(Dense(256, activation='tanh'))
+    model.add(Dense(128, activation='tanh'))
     model.add(Dense(2, activation='softmax'))
 
     if (fromfile == 'none'):
